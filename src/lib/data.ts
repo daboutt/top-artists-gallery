284f1bcf066b4ccc9e8f9e63c75a25288c03b075
daboutt <baotran2115@gmail.com>
2025-12-11 20:48:56 +0700

    feat: add omg

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -2,11 +2,15 @@ import supernatural from '/supernatural.gif';
 import supernatural2 from '/supernatural-2.gif';
 import supernatural3 from '/supernatural-3.gif';
 import ditto from '/ditto.gif';
+import omg from '/omg.gif';
 import newjeans from '/newjeans.gif';
+import omg2 from '/omg-2.gif';
 import ditto2 from '/ditto-2.gif';
 import newjeans2 from '/newjeans-2.gif';
+import omg3 from '/omg-3.gif';
 import ditto3 from '/ditto-3.gif';
 import newjeans3 from '/newjeans-3.gif';
+
 import type { ArtistPreview } from './types';
 
 export const data: Record<string, ArtistPreview[]> = {
@@ -68,6 +72,26 @@ export const data: Record<string, ArtistPreview[]> = {
       offsetX: 390,
       offsetY: -40,
       rotate: -14,
+    },
+  ],
+  omg: [
+    {
+      src: omg,
+      offsetX: -340,
+      offsetY: -20,
+      rotate: 12,
+    },
+    {
+      src: omg2,
+      offsetX: -350,
+      offsetY: 270,
+      rotate: 5,
+    },
+    {
+      src: omg3,
+      offsetX: 450,
+      offsetY: 150,
+      rotate: -10,
     },
   ],
 };
